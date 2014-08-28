452134cc9c9f442d3cf743187a5e21ece59c6c73
Swami Atma <swami@ohlalaweb.com>
2014-08-28 15:27:46 +0200

    Created a member resource to be used by admins and to display the public profile.

---
+++ app/controllers/members_controller.rb
@@ -0,0 +1,62 @@
+class MembersController < ApplicationController
+
+  before_filter :authenticate,            except: :show
+  before_filter :authenticate_with_admin, except: :show
+
+  before_action :set_member, only: [ :show, :edit, :update, :destroy ]
+
+  def index
+    @members = Member.by_recent.paginate(:page => params[:page], :per_page => 50)
+    switch_to_admin_layout
+  end
+
+  def show
+  end
+
+  def new
+    @member = Member.new
+    switch_to_admin_layout
+  end
+
+  def edit
+    switch_to_admin_layout
+  end
+
+  def create
+    @member = Member.new(member_params)
+    if @member.save
+      redirect_to @member, notice: t('members.create.notice')
+    else
+      render action: "new", warning: t('members.create.warning'), layout: 'admin'
+    end
+  end
+
+  def update
+    if @member.update_attributes(member_params)
+      redirect_to @member, notice: t('members.update.notice')
+    else
+      render action: "edit", warning: t('members.update.warning'), layout: 'admin'
+    end
+  end
+
+  def destroy
+    if @member.destroy
+      redirect_to users_path, notice: t('members.destroy.notice')
+    else
+      redirect_to users_path, warning: t('members.destroy.warning')
+    end
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_member
+    @member = Member.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def member_params
+    params.require(:member).permit( :first_name, :last_name, :other_name, :teacher_status )
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
-    resources :details
+
+    # Mailing list records for admin
+    resources :details # deprecated. Should disappear soon.
+
+    # for public viewing and admin editing
+    resources :members
+    # for user to edit and view privately
+    resources :profile
+
   end
 end
