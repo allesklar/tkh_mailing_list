a32adbb5a90bb4593df3ada2808914030bccf02c
Swami Atma <swami@TenThousandHours.eu>
2015-02-16 13:23:05 +0100

    Disabled the sending of individual emails upon receiving a contact message and added some translation strings related to contacts.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,7 +1,7 @@
 class ContactsController < ApplicationController
 
-  before_filter :authenticate,            :except => 'create'
-  before_filter :authenticate_with_admin, :except => 'create'
+  before_action :authenticate,            :except => 'create'
+  before_action :authenticate_with_admin, :except => 'create'
 
   def index
     @contacts = Contact.by_recent.paginate(:page => params[:page], :per_page => 35)
@@ -16,19 +16,26 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.new(contact_params)
     saved = @contact.save
-    sent_email = send_message_to_admin(@contact)
+    # disabling this and replacing it with a daily digest
+    # this is to mitigate the pain associated with contact spams
+    # sent_email = send_message_to_admin(@contact)
+    # if saved && sent_email == 'success'
+    #   redirect_to root_path, notice: t("contacts.create.notice")
+    # elsif saved && sent_email == 'exception'
+    #   flash[:error] = t("contacts.create.warning")
+    #   redirect_to :back
+    # elsif saved && sent_email == 'invalid'
+    #   flash[:error] =  "#{t("contacts.create.warning")} #{t('contacts.create.invalid_email')}"
+    #   redirect_to :back
+    # else
+    #   flash[:error] = t('contacts.create.did_not_reach')
+    #   redirect_to :back
+    # end
 
-    if saved && sent_email == 'success'
+    if saved
       redirect_to root_path, notice: t("contacts.create.notice")
-    elsif saved && sent_email == 'exception'
-      flash[:error] = t("contacts.create.warning")
-      redirect_to :back
-    elsif saved && sent_email == 'invalid'
-      flash[:error] =  "#{t("contacts.create.warning")} #{t('contacts.create.invalid_email')}"
-      redirect_to :back
     else
-      flash[:error] = t('contacts.create.did_not_reach')
-      redirect_to :back
+      redirect_to :back, alert: t('contacts.create.did_not_reach')
     end
   end
 
@@ -39,22 +46,23 @@ class ContactsController < ApplicationController
 
   private
 
-  def send_message_to_admin(contact)
-    # check email validity
-    if valid_email?(contact.sender_email)
-      # Actually send the email to the student
-      begin
-        ContactMailer.message_from_contact_form(contact).deliver
-        return 'success'
-      rescue Exception => e
-        AdminMailer.rescued_exceptions(e, "Some exception occurred while trying to send to site admin a message from contact form").deliver
-        @exception = e
-        return 'exception'
-      end
-    else # for invalid or blank emails
-      return 'invalid'
-    end
-  end
+  #  disabled to be replaced by daily digest to admins
+  # def send_message_to_admin(contact)
+  #   # check email validity
+  #   if valid_email?(contact.sender_email)
+  #     # Actually send the email to the student
+  #     begin
+  #       ContactMailer.message_from_contact_form(contact).deliver
+  #       return 'success'
+  #     rescue Exception => e
+  #       AdminMailer.rescued_exceptions(e, "Some exception occurred while trying to send to site admin a message from contact form").deliver
+  #       @exception = e
+  #       return 'exception'
+  #     end
+  #   else # for invalid or blank emails
+  #     return 'invalid'
+  #   end
+  # end
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def contact_params
