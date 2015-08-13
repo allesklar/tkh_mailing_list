cd8322fc11df75e1fbdbdc7780a0c2c05dc335a5
Swami Atma <swami@TenThousandHours.eu>
2015-08-13 08:02:33 +0200

    Fixed up the logic flow to the newsletter signup process. It needs to be refactored.

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -1,16 +1,38 @@
 class SignupsController < ApplicationController
 
+  # FIXME
+  # main method should be named 'signup_for_newsletter' to be correct
+  # big refactoring will be needed when implementing multiple newsletter feature
   def email_verification
     unless params[:user][:email].blank?
       @user = User.find_or_initialize_by(email: params[:user][:email])
-      set_email_validation_token
-      @user.allow_newsletter = true
-      @user.allow_daily_digests = true
-      @user.save ? @validation_ready=true : @validation_ready=false
-      SignupMailer.verification_email(@user).deliver
-      Activity.create doer_id: @user.id, message: "signed up for the newsletter. Email: #{@user.email}"
-    else
-     redirect_to root_path, alert: 'Your email cannot be blank. Please enter your email address and try again'
+
+      if @user.email_validated? == false
+
+        set_email_validation_token
+        @user.allow_newsletter = true
+        @user.allow_daily_digests = true
+        @user.save ? @validation_ready=true : @validation_ready=false
+        SignupMailer.verification_email(@user).deliver
+        Activity.create doer_id: @user.id, message: "signed up for the newsletter. Email: #{@user.email}"
+        # render the email_verification view
+
+      else # the email address has already been validated
+
+        if @user.allow_newsletter?
+          redirect_to root_path, notice: "You are already subscribed to the newsletter. Nothing to do but wait for the next issue."
+        else # the user just subscribed for the first time.
+          @user.allow_newsletter = true
+          @user.allow_daily_digests = true
+          @user.save
+          Activity.create doer_id: @user.id, message: "signed up for the newsletter. Email: #{@user.email}"
+          redirect_to root_path, notice: "Thank you for subscribing to the newsletter."
+        end
+
+      end
+
+    else  # the email field is blank
+       redirect_to :back, alert: 'Your email cannot be blank. Please enter your email address and try again'
     end
   end
 
